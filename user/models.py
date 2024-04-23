2165ea7be7e0ca9580d27f615d14952dd380f21d
Anton Tkachuk <tkacuk2291@gmail.com>
2024-04-23 19:23:23 +0300

    remove unesful import

--- user/models.py
+++ user/models.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext as _
-from django.contrib import auth
 from django.contrib.auth.base_user import (
     AbstractBaseUser,
     BaseUserManager as DjangoUserManager,
